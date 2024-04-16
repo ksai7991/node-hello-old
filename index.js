0a68329be2e7d54d7bbbe10a448f685289e9cc0c
ksai7991 <76088923+ksai7991@users.noreply.github.com>
2024-04-16 20:27:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,40 +6,6 @@ const server = http.createServer((req, res) => {
   const msg = 'Hello Node!\n';
   res.write(msg);
 
-  // Options for the HTTP request
-  const options = {
-    hostname: 'internal-service',
-    port: 8080,
-    path: '/',
-    method: 'GET'
-  };
-
-  // Make the HTTP request
-  const reqInternal = http.request(options, (resInternal) => {
-    let data = '';
-
-    // A chunk of data has been received.
-    resInternal.on('data', (chunk) => {
-      data += chunk;
-    });
-
-    // The whole response has been received.
-    resInternal.on('end', () => {
-      res.write(data); // Write the response from internal-service to the main response
-      res.end(); // End the main response
-    });
-  });
-
-  // Handle errors for internal HTTP request
-  reqInternal.on('error', (error) => {
-    console.error(`Error making internal HTTP request: ${error.message}`);
-    res.end(); // End the main response even if there's an error
-  });
-
-  // End the internal HTTP request
-  reqInternal.end();
-});
-
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`);
 });
