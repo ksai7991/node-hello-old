09cf42291bad083c04caa5781fdefcf64f986e82
ksai7991 <76088923+ksai7991@users.noreply.github.com>
2024-04-26 12:05:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
+let count = 0;
 
 const server = http.createServer((req, res) => {
-  console.log('Received request');
+  count++; // Increment the request count
+  console.log(`Request number ${count} received`);
 
   // Delay response for 10 seconds
   setTimeout(() => {
     res.statusCode = 200;
-    const msg = 'Hello Node!\n';
+    const msg = `Hello Node!\n`;
     res.write(msg);
 
     // Options for the HTTP request
@@ -20,7 +22,6 @@ const server = http.createServer((req, res) => {
 
     // Make the HTTP request
     const reqInternal = http.request(options, (resInternal) => {
-      console.log('Internal request made');
 
       let data = '';
 
@@ -31,7 +32,6 @@ const server = http.createServer((req, res) => {
 
       // The whole response has been received.
       resInternal.on('end', () => {
-        console.log('Internal request completed');
         res.write(data); // Write the response from internal-service to the main response
         res.end(); // End the main response
       });
@@ -39,14 +39,12 @@ const server = http.createServer((req, res) => {
 
     // Handle errors for internal HTTP request
     reqInternal.on('error', (error) => {
-      console.error(`Error making internal HTTP request: ${error.message}`);
       res.end(); // End the main response even if there's an error
     });
 
     // End the internal HTTP request
     reqInternal.end();
-  }, 10000); // 10 seconds delay
-
+  }, 1000); // 10 seconds delay
 });
 
 server.listen(port, () => {
